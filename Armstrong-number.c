39370c5d1babdba848142a508983f8c8b6965089
Manas Jaiswal <52818738+mjaiswal141@users.noreply.github.com>
2021-10-09 23:33:23 +0530

    Add files via upload

---
+++ Armstrong-number.c
@@ -0,0 +1,20 @@
+// Armstrong-number
+#include<stdio.h>  
+int main()    
+{    
+int n,r,sum=0,temp;    
+printf("Enter the number = ");    
+scanf("%d",&n);    
+temp=n;    
+while(n>0)    
+{    
+r=n%10;    
+sum=sum+(r*r*r);    
+n=n/10;    
+}    
+if(temp==sum)    
+printf("Armstrong number");    
+else    
+printf("Not a armstrong number");    
+return 0;  
+}
